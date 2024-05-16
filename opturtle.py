fb7ce543cb8d994150034df287dd79f56717b360
Joonthom <Joonthom@gmail.com>
2024-05-16 13:07:55 +0200

    d

---
+++ opturtle.py
@@ -0,0 +1,10 @@
+import turtle
+
+t = turtle.Turtle()
+t.speed(1)
+
+t.pendown()
+t.goto(0, -100)
+t.goto(-100, -100)
+t.goto(-100, 0)
+t.goto(0, 0)
--- turtle.py
+++
@@ -1,5 +0,0 @@
-from turtle import Screen, Turtle
-from random import randint
-
->>> s = turtle.getscreen()
-turtle.exitonclick()
