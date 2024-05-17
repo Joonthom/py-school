56afae42265c1608de4d2aa4483d5b1e21550bb4
Joonthom <Joonthom@gmail.com>
2024-05-17 17:06:08 +0200

    Update opturtle.py

--- opturtle.py
+++ opturtle.py
@@ -1,23 +1,34 @@
-import turtle
+# import turtle
 
-#s = turtle.getscreen()
-#turtle.exitonclick()
+# #s = turtle.getscreen()
+# #turtle.exitonclick()
 
-t = turtle.Turtle()
-t.speed(1)
-t.pendown()
+# t = turtle.Turtle()
+# t.speed(1)
+# t.pendown()
 
-#Square
+# #Square
 # t.goto(0, -100)
 # t.goto(-100, -100)
 # t.goto(-100, 0)
 # t.goto(0, 0)
 
-#Triangle
-# t.goto(-100,100)
-# t.goto(100,100)
-# #t.goto(0,0)
-# t.home()
+# #Triangle
+# # t.goto(-100,100)
+# # t.goto(100,100)
+# # #t.goto(0,0)
+# # t.home()
 
-#Circle met achtergrond
-turtle.bgcolor("blue")
+# # #Circle met achtergrond
+# # turtle.bgcolor("blue")
+
+
+import turtle
+
+s = turtle.getscreen()
+turtle.exitonclick()
+
+t = turtle.Turtle()
+t.pendown()
+t.rt(90)
+t.fd(100)
