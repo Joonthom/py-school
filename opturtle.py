a43972f497fd105523b165600e9ef29ba6b1f19e
Jonathan <Joonthom@gmail.com>
2024-09-10 12:59:59 +0200

    Update opturtle.py

--- opturtle.py
+++ opturtle.py
@@ -1,9 +1,19 @@
 import turtle
 
+<<<<<<< Updated upstream
 t = turtle.Turtle()
 t.speed(1)
 t.pendown()
 
+=======
+s = turtle.getscreen()
+turtle.exitonclick()
+
+t = turtle.Turtle()
+t.speed(1)
+t.pendown()
+
+>>>>>>> Stashed changes
 #Square
 t.goto(0, -100)
 t.goto(-100, -100)
@@ -24,6 +34,18 @@ t.goto(0, 0)
 # t.rt(90)
 # t.fd(100)
 
+<<<<<<< Updated upstream
 #Dit als laatste lijn houden
 s = turtle.getscreen()
 turtle.exitonclick()
+=======
+# import turtle
+
+# s = turtle.getscreen()
+# turtle.exitonclick()
+
+# t = turtle.Turtle()
+# t.pendown()
+# t.rt(90)
+# t.fd(100)
+>>>>>>> Stashed changes
