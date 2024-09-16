6d67a07f856bf0eab27ca2d5530c8f0e99e74bf0
Jonathan <Joonthom@gmail.com>
2024-09-16 15:30:43 +0200

    Update opturtle.py

--- opturtle.py
+++ opturtle.py
@@ -1,51 +1,29 @@
 import turtle
 
-<<<<<<< Updated upstream
 t = turtle.Turtle()
 t.speed(1)
-t.pendown()
+#t.pendown()
 
-=======
-s = turtle.getscreen()
-turtle.exitonclick()
-
-t = turtle.Turtle()
-t.speed(1)
-t.pendown()
-
->>>>>>> Stashed changes
-#Square
-t.goto(0, -100)
-t.goto(-100, -100)
-t.goto(-100, 0)
-t.goto(0, 0)
+# #Square
+# t.goto(0, -100)
+# t.goto(-100, -100)
+# t.goto(-100, 0)
+# t.goto(0, 0)
 
 # #Triangle
-# # t.goto(-100,100)
-# # t.goto(100,100)
-# # #t.goto(0,0)
-# # t.home()
+# t.goto(-100,100)
+# t.goto(100,100)
+# t.goto(0,0)
+# t.home()
 
 # # #Circle met achtergrond
 # # turtle.bgcolor("blue")
 
-# t = turtle.Turtle()
-# t.pendown()
-# t.rt(90)
-# t.fd(100)
+t.pendown()
+t.fd(100)
+t.rt(90)
+t.fd(100)
 
-<<<<<<< Updated upstream
 #Dit als laatste lijn houden
 s = turtle.getscreen()
 turtle.exitonclick()
-=======
-# import turtle
-
-# s = turtle.getscreen()
-# turtle.exitonclick()
-
-# t = turtle.Turtle()
-# t.pendown()
-# t.rt(90)
-# t.fd(100)
->>>>>>> Stashed changes
